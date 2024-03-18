a60d0419361e9c3d9c3aeba74a235f6502e26443
arshawalia71 <60817328+arshawalia71@users.noreply.github.com>
2024-03-18 16:52:48 +0530

    Update home2.py

--- cta/pages_rec/home2.py
+++ cta/pages_rec/home2.py
@@ -3,7 +3,7 @@ import utils as utl
 
 #this is the function that is executed once the user is authenticated
 def load_view():
-    query_params=st.experimental_get_query_params() 
+    # query_params=st.experimental_get_query_params() 
     # if len(query_params) > 0:
     #     if 'username' in query_params:
     #         query_params = query_params['username'][0] #gets the username from the mscall graph after authentication
@@ -14,7 +14,7 @@ def load_view():
     st.header("DCPS")
     st.write("Adobe's Document Cloud Professional Services refers to a range of consulting and implementation services offered by Adobe to assist organizations in deploying and optimizing their use of Adobe Document Cloud. These services are designed to help organizations maximize the value and effectiveness of Adobe Document Cloud within their specific business environment.The Professional Services team at Adobe works closely with customers to understand their unique requirements, challenges, and goals. They provide expertise, guidance, and support throughout the implementation process to ensure a successful deployment of Adobe Document Cloud.")
 #currently not being used can be used later to send username to different pages
-"""def get_name():
-    return st.session_state.username"""
+# """def get_name():
+#     return st.session_state.username"""
 
 
